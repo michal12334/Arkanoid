89f573bbe5f5b6aefb89ae09192aa64733b55b8e
michal <michal1842001@gmail.com>
2021-02-12 21:07:12 +0100

    add empty update method

--- oneColorButtonWithText.cpp
+++ oneColorButtonWithText.cpp
@@ -8,6 +8,8 @@ OneColorButtonWithText::OneColorButtonWithText(Color color, Text text) :OneColor
 	this->text = text;
 }
 
+void OneColorButtonWithText::update() {}
+
 void OneColorButtonWithText::draw(RenderTarget &target, RenderStates state) const {
 	target.draw(shape, state);
 	target.draw(text, state);
--- oneColorButtonWithText.h
+++ oneColorButtonWithText.h
@@ -6,6 +6,7 @@ class OneColorButtonWithText :public OneColorButton {
 public:
 	OneColorButtonWithText();
 	OneColorButtonWithText(sf::Color color, sf::Text text);
+	void update();
 
 private:
 	void draw(sf::RenderTarget &target, sf::RenderStates state) const override;
