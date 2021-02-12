a867d1f4c776100e750ebe08ac84bee6009728f7
michal <michal1842001@gmail.com>
2021-02-12 21:35:03 +0100

    add setTextPosition method to oneColorButtonWithText class

--- oneColorButtonWithText.cpp
+++ oneColorButtonWithText.cpp
@@ -10,6 +10,23 @@ OneColorButtonWithText::OneColorButtonWithText(Color color, Text text) :OneColor
 
 void OneColorButtonWithText::update() {}
 
+void OneColorButtonWithText::setTextPosition(Vector2f position) {
+	textPosition = position;
+	Vector2f realTextPosition;
+	realTextPosition.x = textPosition.x + this->position.x;
+	realTextPosition.y = textPosition.y + this->position.y;
+	text.setPosition(realTextPosition);
+}
+
+void OneColorButtonWithText::setPosition(Vector2f position) {
+	this->position = position;
+	this->shape.setPosition(position);
+	Vector2f realTextPosition;
+	realTextPosition.x = textPosition.x + this->position.x;
+	realTextPosition.y = textPosition.y + this->position.y;
+	text.setPosition(realTextPosition);
+}
+
 void OneColorButtonWithText::draw(RenderTarget &target, RenderStates state) const {
 	target.draw(shape, state);
 	target.draw(text, state);
--- oneColorButtonWithText.h
+++ oneColorButtonWithText.h
@@ -7,8 +7,11 @@ public:
 	OneColorButtonWithText();
 	OneColorButtonWithText(sf::Color color, sf::Text text);
 	void update();
+	void setTextPosition(sf::Vector2f position);
+	void setPosition(sf::Vector2f position);
 
 private:
 	void draw(sf::RenderTarget &target, sf::RenderStates state) const override;
 	sf::Text text;
+	sf::Vector2f textPosition;
 };
