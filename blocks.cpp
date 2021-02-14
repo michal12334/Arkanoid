271af4e568468a066cd3bcd1477bdf837ceab2bf
michal <michal1842001@gmail.com>
2021-02-14 16:36:16 +0100

    create some methods to Blocks class

--- blocks.cpp
+++ blocks.cpp
@@ -6,6 +6,22 @@ void Blocks::addBlock(Block block) {
 	blocks.push_back(block);
 }
 
+Block *Blocks::getBlock(int index) {
+	return &blocks[index];
+}
+
+int Blocks::size() {
+	return blocks.size();
+}
+
+void Blocks::update() {
+	for(int i = 0; i < size(); i++) {
+		if(!blocks[i].getEnable()) {
+			blocks.erase(blocks.begin() + i);
+		}
+	}
+}
+
 void Blocks::draw(RenderTarget &target, RenderStates state) const {
 	for(const auto& block : blocks) {
 		target.draw(block, state);
--- blocks.h
+++ blocks.h
@@ -6,9 +6,11 @@
 class Blocks :public sf::Drawable {
 public:
 	void addBlock(Block block);
+	Block *getBlock(int index);
+	int size();
+	void update();
 
 private:
 	void draw(sf::RenderTarget &target, sf::RenderStates state) const override;
 	std::vector<Block> blocks;
-
 };
